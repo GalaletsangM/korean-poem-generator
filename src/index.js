a0803f8fbc6b12f4776ee5f1b0cb161eb6512903
GalaletsangM <preciousmafoko@gmail.com>
2024-01-24 23:01:03 -0600

    editted javascript

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ function generatePoem(event) {
   let userInput = document.querySelector("#user-input");
 
   let key = "tb8746f8a332o55bf03481bf03f90fe4";
-  let prompt = `write a short poem in korean about the word "${userInput.value}", also write in the way the whole poem should be prnounced. Use html format, example : <p>hello </p>.`;
-  let context = "you are a great poet and you like writing short poems. Sign the poem with 'SheCodes AI' inside a <strong> element at the bottom";
+  let prompt = `write a short poem in korean about the word "${userInput.value}", also write in the way the whole poem should be prnounced. Use html format, example : <p>hello</p>`;
+  let context =
+    "you are a great poet and you like writing short poems. Sign the poem with 'SheCodes AI' inside a <strong> element at the bottom. ";
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
   axios.get(url).then(displayPoem);
